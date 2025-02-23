dad1edd841f11bd939f9866edff4fa40ed440d98
Miguel Soares Francisco <miguel.soares.francisco@ua.pt>
2025-02-23 22:35:13 +0000

    Update carousel.jsx

--- microsite-main/src/pages/carousel.jsx
+++ microsite-main/src/pages/carousel.jsx
@@ -3,8 +3,7 @@ import ImageCarousel from '@site/src/components/ImageCarousel';
 // ... importe todas as imagens ...
 
 const carouselItems = [
-  { url: Mockup1, caption: 'Página 1' },
-  { url: Mockup2, caption: 'Página 2' },
+ 
   // ... crie objetos para todas as imagens ...
 ];
 
